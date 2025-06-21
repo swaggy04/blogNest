342aff93d6c92ac7aa5cad82304a767b03482f31
swagatpadhi <litan3104@gmail.com>
2025-06-22 01:19:48 +0530

    changes

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -8,6 +8,7 @@ import { Card } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { fetchArticleByQuery } from "@/lib/query/fetch-article";
 import Link from "next/link";
+import AllArticlesPageSkeleton from "@/components/article/aLL-articles-page-skeleton";
 
 type SearchPageProps = {
   searchParams: { search?: string; page?: string };
@@ -93,40 +94,3 @@ const page: React.FC<SearchPageProps> = async ({ searchParams }) => {
 
 export default page;
 
-export function AllArticlesPageSkeleton() {
-  return (
-    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
-      {Array.from({ length: 3 }).map((_, index) => (
-        <Card
-          key={index}
-          className="group relative overflow-hidden transition-all hover:shadow-lg"
-        >
-          <div className="p-6">
-            {/* Article Image Skeleton */}
-            <Skeleton className="mb-4 h-48 w-full rounded-xl bg-gradient-to-br from-purple-100/50 to-blue-100/50 dark:from-purple-900/20 dark:to-blue-900/20" />
-
-            {/* Article Title Skeleton */}
-            <Skeleton className="h-6 w-3/4 rounded-lg" />
-
-            {/* Article Category Skeleton */}
-            <Skeleton className="mt-2 h-4 w-1/2 rounded-lg" />
-
-            {/* Author & Metadata Skeleton */}
-            <div className="mt-6 flex items-center justify-between">
-              <div className="flex items-center gap-3">
-                {/* Author Avatar Skeleton */}
-                <Skeleton className="h-8 w-8 rounded-full" />
-
-                {/* Author Name Skeleton */}
-                <Skeleton className="h-4 w-20 rounded-lg " />
-              </div>
-
-              {/* Date Skeleton */}
-              <Skeleton className="h-4 w-24 rounded-lg " />
-            </div>
-          </div>
-        </Card>
-      ))}
-    </div>
-  );
-}
